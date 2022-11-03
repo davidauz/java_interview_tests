3d31bd06463bc89080d2140f34d064eec2ab213f
Davide Saccon <davide.saccon@gmail.com>
2022-11-03 17:09:29 +0800

    Added HashSet to Collections

--- src/generic/collections.java
+++ src/generic/collections.java
@@ -1,12 +1,22 @@
 package generic;
 
-import java.util.AbstractMap;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class collections {
     public void go() {
         test_hash_map();
+        test_hash_set();
+    }
+
+    private void test_hash_set() {
+        Collection<String> string_hash_set = new HashSet<>();
+        System.out.println("HashSet");
+        string_hash_set.add("ABBA");
+        string_hash_set.add("BABBA");
+        string_hash_set.add("ABBAB");
+        System.out.println("Before adding 'BABBA' to : "+string_hash_set+" (again)");
+        string_hash_set.add("BABBA");
+        System.out.println("HashSet:"+string_hash_set+"; no duplicates!");
 
     }
 
@@ -14,11 +24,12 @@ public class collections {
         //public abstract class AbstractMap<K, V> implements Map<K, V>
         //public interface Map<K, V>
         Map<String, String> hash_map = new HashMap<>(); // public class HashMap<K, V> extends AbstractMap<K, V> implements Map<K, V>, Cloneable, Serializable
+        System.out.println("HashMap");
         hash_map.put("1", "ABBA");
         hash_map.put("2", "BABBA");
         hash_map.put("3", "ABBAB");
-        System.out.println("Before: "+hash_map);
+        System.out.println("Adding '2, ZZZ': to "+hash_map);
         hash_map.put("2", "ZZZ");
-        System.out.println("After:"+hash_map+"; no duplicates!");
+        System.out.println("No duplicate indexes: "+hash_map+"\n");
     }
 }
