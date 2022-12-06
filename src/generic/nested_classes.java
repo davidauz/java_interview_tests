e46a3fc8474d03d7aeb9708d0b03b794c5e2b135
Davide Saccon <davide.saccon@gmail.com>
2022-12-06 09:56:10 +0800

    adjusted nested class example

--- src/generic/nested_classes.java
+++ src/generic/nested_classes.java
@@ -1,19 +1,24 @@
 package generic;
 
 public class nested_classes {
-    static class nested_class{
-        static void speak(){System.out.println("I am a plain nested class");}
-    }
+
     public interface anonymous_class{ // technically this is a nonstatic member interface?
         void speak();
     }
-    public static class static_member_class{
+
+    public static class static_member_class{ // static member class
         public static void speak(){System.out.println("I am a Static Member Class");}
     }
+
     public class non_static_member_class{
         public static void speak(){System.out.println("I am a NON Static Member Class");}
     }
+
     public void go(){
+        class local_class {
+            static void speak(){System.out.println("I am a local nested class");}
+        }
+
         System.out.println("There are four types of nested class: \n" +
                         "#Static member classes \n" +
                         "#Nonstatic member classes\n" +
@@ -21,7 +26,7 @@ public class nested_classes {
                         "#Local classes\n");
         static_member_class.speak();
         non_static_member_class.speak();
-        nested_class.speak();
+        local_class.speak();
         new anonymous_class() {
             @Override
             public void speak() {
